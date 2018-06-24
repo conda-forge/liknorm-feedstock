fc4c323c8a37231ddb980c34d8e4479a00601f28
Danilo Horta <danilo.horta@gmail.com>
2018-06-25 00:36:15 +0100

    librayr prefix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ cd build
 cmake -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-         -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
