c42f1c6c72f97ffcb0bac7ae06e9a53f7f2091d9
Danilo Horta <danilo.horta@gmail.com>
2018-06-25 01:35:40 +0100

    MD to MT

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,18 +2,26 @@ set LIB=%LIBRARY_LIB%;.\lib;%LIB%
 set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
+set CFLAGS=%CFLAGS:MD=MT%
+set CXXFLAGS=%CXXFLAGS:MD=MT%
+
 mkdir build
 cd build
 
 cmake -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_BUILD_TYPE=Release ^
-         -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
          -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --config Release --target install
+:: Build.
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-ctest
+:: Test.
+ctest -C Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
