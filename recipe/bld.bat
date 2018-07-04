bb908737f70f99a69f56ca93813963d56907edcf
Danilo Horta <danilo.horta@gmail.com>
2018-07-04 10:35:02 +0100

    nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,14 +8,14 @@ set CXXFLAGS=%CXXFLAGS:MD=MT%
 pushd . && mkdir build && cd build
 if errorlevel 1 exit 1
 
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -G "NMake Makefiles" ^
          -D CMAKE_BUILD_TYPE=Release ^
          -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: Build.
-cmake --build . --config Release
+nmake
 if errorlevel 1 exit 1
 
 :: Test.
@@ -23,7 +23,7 @@ ctest -C Release
 if errorlevel 1 exit 1
 
 :: Install.
-cmake --build . --config Release --target install
+nmake install
 if errorlevel 1 exit 1
 
 popd && rd /q /s build
