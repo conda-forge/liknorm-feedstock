2c713847bc181fd16be9e85b058a1642c7343c7b
Danilo Horta <danilo.horta@gmail.com>
2018-07-04 11:37:13 +0100

    export

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,15 +2,13 @@ set LIB=%LIBRARY_LIB%;.\lib;%LIB%
 set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-set CFLAGS=%CFLAGS:MD=MT%
-set CXXFLAGS=%CXXFLAGS:MD=MT%
-
 pushd . && mkdir build && cd build
 if errorlevel 1 exit 1
 
 cmake -G "NMake Makefiles" ^
          -D CMAKE_BUILD_TYPE=Release ^
          -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+         -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
@@ -19,7 +17,7 @@ nmake
 if errorlevel 1 exit 1
 
 :: Test.
-ctest -C Release
+nmake test
 if errorlevel 1 exit 1
 
 :: Install.
