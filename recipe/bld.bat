fe810cd654a023d60fd9d1d40b8f99ff11e9c171
Danilo Horta <danilo.horta@gmail.com>
2018-07-04 06:24:26 +0100

    windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,18 +2,21 @@ set LIB=%LIBRARY_LIB%;.\lib;%LIB%
 set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-mkdir build
-cd build
+pushd . && mkdir build && cd build
+if errorlevel 1 exit 1
 
 cmake -G "%CMAKE_GENERATOR%" ^
-         -DCMAKE_BUILD_TYPE=Release ^
-         -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-         -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
+         -D CMAKE_BUILD_TYPE=Release ^
+         -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
+         -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-ctest
+ctest -V --output-on-failure -C Release
 if errorlevel 1 exit 1
+
+popd && rd /q /s build
