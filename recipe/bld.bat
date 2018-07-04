f0ffd668810bcda8185837950812d7a0f1dfde17
Danilo Horta <danilo.horta@gmail.com>
2018-07-04 07:32:35 +0100

    copycat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,16 +10,20 @@ if errorlevel 1 exit 1
 
 cmake -G "%CMAKE_GENERATOR%" ^
          -D CMAKE_BUILD_TYPE=Release ^
-         -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-         -D CMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
          -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --config Release --target install
+:: Build.
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-ctest -V --output-on-failure -C Release
+:: Test.
+ctest -C Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 popd && rd /q /s build
