c9bdee954c69813814e08cc771298ab63f303641
Danilo Horta <danilo.horta@gmail.com>
2018-07-04 06:40:12 +0100

    win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ if errorlevel 1 exit 1
 cmake -G "%CMAKE_GENERATOR%" ^
          -D CMAKE_BUILD_TYPE=Release ^
          -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-         -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+         -D CMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
          -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
          %SRC_DIR%
 if errorlevel 1 exit 1
