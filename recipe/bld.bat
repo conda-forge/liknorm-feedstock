7b1fff6e8e5e8df5612c9b428456d87c5a31593e
Danilo Horta <danilo.horta@gmail.com>
2018-07-05 07:31:04 +0100

    update

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,9 @@
-set LIB=%LIBRARY_LIB%;.\lib;%LIB%
-set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-set "CFLAGS=-MD"
-
 pushd . && mkdir build && cd build
 if errorlevel 1 exit 1
 
 cmake -G "NMake Makefiles" ^
          -D CMAKE_BUILD_TYPE=Release ^
          -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-         -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
